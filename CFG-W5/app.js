b373f6382a3dcc2aa29dda44c51a540d0dd62165
Mwen-A <45672207+Mwen-A@users.noreply.github.com>
2020-08-23 21:40:17 +0200

    updated with CSS

---
+++ CFG-W5/app.js
@@ -0,0 +1,68 @@
+
+
+
+
+// document.write = ("Hello");
+
+// let calc = prompt("Would you like to make a calculation?");
+// if (calc == "yes"){
+//     alert("Lets calculate!")
+// }else{
+//     alert("Bye");
+// }
+
+
+function addition(a,b){
+var a = prompt("Input the first number: ");
+var b = prompt("Input the the second number: ");
+var c= +a + +b;  //for some reason processes it as a string instead of integers
+    alert(c);
+}
+
+function subtract(a,b){
+var a = prompt("Input the first number: ");
+var b = prompt("Input the the second number: ");
+var c = a - b;
+    alert(c);
+}
+
+function multi(a,b){
+var a = prompt("Input the first number: ");
+var b = prompt("Input the the second number: ");
+var c = a * b;
+    alert(c);
+}
+
+function divide(a,b){
+var a = prompt("Input the first number: ");
+var b = prompt("Input the the second number: ");
+var c = a / b;
+    alert(c);
+}
+
+function exponent(a, b){
+var a = prompt("Input the base number: ");
+var b = prompt("Input the exponent power: ");
+var c = Math.pow(a, b)
+    alert(c);
+}
+
+function square(a){
+var a = prompt("Input number: ");
+var c = Math.sqrt(a);
+    alert(c);
+}
+
+
+// // function alternatives(){
+// //     var calc = prompt("Would you like to make a calculation?");
+
+// //     if (calc == "yes"){
+// //         return calculation();
+// //     }else{
+// //         alert("Bye");
+// //     }
+// // }
+
+
+
